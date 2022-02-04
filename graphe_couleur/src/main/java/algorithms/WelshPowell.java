ec2aeabb6cce6fdcd51320c4904f4fe33d3b34c0
Zhou Alexandre <zhoualexandre@gmail.com>
2022-02-04 17:40:02 +0100

    Correction d'un bug

--- graphe_couleur/src/main/java/algorithms/WelshPowell.java
+++ graphe_couleur/src/main/java/algorithms/WelshPowell.java
@@ -6,7 +6,7 @@ import graphs.Vertex;
 
 public class WelshPowell {
     
-    public ArrayList<Vertex> selectionSort(ArrayList<Vertex> list) {
+    public static ArrayList<Vertex> selectionSort(ArrayList<Vertex> list) {
         for(int i = 0 ; i < list.size() -1 ; i ++) {
             int index = i;
             for(int j = i + 1 ; j < list.size() ; j++) {
@@ -22,21 +22,21 @@ public class WelshPowell {
         return list;
     }
 
-    public ArrayList<Vertex> welshPowell(ArrayList<Vertex> list){
+    public static ArrayList<Vertex> welshPowell(ArrayList<Vertex> list){
         ArrayList<Vertex> temp;
         temp = selectionSort(list);
         for(int i = 0 ; i < list.size() ; i++){
-            if(i % 7 == 0){
-                list.get(i).setColor(Color.BLUE);
+            if(i % 6 == 0){
+                temp.get(i).setColor(Color.BLUE);
             }
-            else if(i % 7 == 2 || i % 7 == 6){
-                list.get(i).setColor(Color.RED);
+            else if(i % 6 == 1 || i % 6 == 5){
+                temp.get(i).setColor(Color.RED);
             }
-            else if(i % 7 == 3 || i % 7 == 5){
-                list.get(i).setColor(Color.GREEN);
+            else if(i % 6 == 2|| i % 6 == 4){
+                temp.get(i).setColor(Color.GREEN);
             }
-            else if(i % 7 == 4){
-                list.get(i).setColor(Color.YELLOW);
+            else if(i % 6 == 3){
+                temp.get(i).setColor(Color.YELLOW);
             }
             else{
                 System.out.println("Erreur dans le code de changement de couleur");
